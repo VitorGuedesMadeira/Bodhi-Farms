c4f4e15243f968df2d92ed09b19bcc762e1703b0
VitorGuedesMadeira <vgm_rox@hotmail.com>
2023-01-24 19:57:32 -0300

    Add different sections for first page and scroll bar

--- src/components/component1/Component1.js
+++ src/components/component1/Component1.js
@@ -5,7 +5,21 @@ import './Component1.css';
 
 const Component1 = () => (
   <div>
-    Component1
+    <section className='section1-container'>
+      1
+    </section>
+
+    <section className='section2-container'>
+      2
+    </section>
+
+    <section className='section3-container'>
+      3
+    </section>
+
+    <section className='section4-container'>
+      4
+    </section>
   </div>
 );
 
