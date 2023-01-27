f437955924af8ba4ac5ead1610ff6de5732d8bf1
VitorGuedesMadeira <vgm_rox@hotmail.com>
2023-01-27 00:27:26 -0300

    Add last styles

--- src/components/component1/Component1.js
+++ src/components/component1/Component1.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 // styles
 import './Component1.css';
@@ -6,12 +7,17 @@ import './Component1.css';
 const Component1 = () => (
   <div>
     <section className='section1-container'>
-      <h1>WE KNOW CANNABIS.</h1>
-      <h2>LICENSING. DESIGN. BUILD. SYSTEMS. OPERATIONS.</h2>
+      <h1>Welcome to Bodhi Farm Hydroponics Cannabis Lab</h1>
     </section>
 
     <section className='section2-container'>
-      2
+      <h2>Who are we?</h2>
+      <p>We are a fully Licensed  Local Producer of premium craft cannabis located in Lamphun.
+      We strive to produce consistent superior quality, pesticide-free, affordable cannabis. We achieve this by growing without soil. We treat our water on site through a three step process of reverse osmosis, distillation, and softening.</p>
+      <p>We are currently working in conjunction with both local, and international universities to conduct   research, and assist in the implementation of both modern, and ancient methods of sustainable agriculture.</p>
+      <p>Everyone is welcome to our farm! Get high!. Save the World!</p>
+
+      <NavLink to="option1">Learn More</NavLink>
     </section>
 
     <section className='section3-container'>
